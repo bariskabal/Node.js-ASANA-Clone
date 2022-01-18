931a9324f982f8ec44e88dd6af64f6e33f36da06
bariskabal <bariskabal12@gmail.com>
2022-01-18 18:20:31 +0300

    Created user | password hash

---
+++ v1/src/scripts/utils/helper.js
@@ -0,0 +1,10 @@
+const CryptoJS = require("crypto-js")
+
+const passwordToHash = (password) => {
+    return CryptoJS.HmacSHA256(password,CryptoJS.HmacSHA1(password,process.env.PASSWORD_HASH).toString()).toString()
+}
+const 
+
+module.exports = {
+    passwordToHash
+}
